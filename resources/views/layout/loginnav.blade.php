a9c4391633456cd82ec7340d0db2c0be4d9228d6
deejacodes <tomeldendjaninetara@gmail.com>
2024-02-20 10:48:39 +0800

    Separate Website and Login nav
    Separated the navbar for login and when already logged in

---
+++ resources/views/layout/loginnav.blade.php
@@ -0,0 +1,8 @@
+<a href="#" class="logo no-underline"><img src="{{asset('images/COA CAR logo.png')}}" alt="" class="logo"><span>COA-CAR</span></a>
+
+<div class="main">
+    <a href="#" class="user active no-underline"><i class="ri-user-line"></i>LOGIN</a>
+    <a href="#" class="no-underline">SIGNUP</a>
+    <div class="bx bx-menu" id="menu-icon"></div>
+</div>
+
--- resources/views/layout/websitenav.blade.php
+++ resources/views/layout/websitenav.blade.php
@@ -1,13 +1,11 @@
 <a href="#" class="logo no-underline"><img src="{{asset('images/COA CAR logo.png')}}" alt="" class="logo"><span>COA-CAR</span></a>
 
 <ul class="navbar">
-    <li><a href="#" class="active no-underline">TEST</a></li>
-    <li><a href="#" class="no-underline">TEST</a></li>
-    <li><a href="#" class="no-underline">TEST</a></li>
-    <li><a href="#" class="no-underline">TEST</a></li>
-    <li><a href="#" class="no-underline">TEST</a></li>
+    <li><a href="#" class="active no-underline">HOME</a></li>
+    <li><a href="#" class="no-underline">GYM</a></li>
+    <li><a href="#" class="no-underline">DORM</a></li>
 </ul>
-dada
+
 <div class="main">
     <a href="#" class="user active no-underline"><i class="ri-user-line"></i>LOGIN</a>
     <a href="#" class="no-underline">SIGNUP</a>
