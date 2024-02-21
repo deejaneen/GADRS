a84e42f62b8f3185b050385933627e6f23f2c998
deejacodes <tomeldendjaninetara@gmail.com>
2024-02-21 15:08:05 +0800

    Fixing navbar alignment

--- resources/views/layout/loginnav.blade.php
+++ resources/views/layout/loginnav.blade.php
@@ -23,21 +23,19 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav centered">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">HOME
-                <span class="visually-hidden">(current)</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('gym') }}">GYM</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('dorm') }}">DORM</a>
-            </li>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">HOME
+                    <span class="visually-hidden">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('gym') }}">GYM</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('dorm') }}">DORM</a>
+                </li>
             </ul>
-        </div>
-        <div class="main">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
                     <a href="#" class="user active no-underline"><i class="ri-user-line"></i>LOGIN</a>
@@ -48,6 +46,6 @@
             </ul>
         </div>
     </div>
-  </nav>
+</nav>
 
   
