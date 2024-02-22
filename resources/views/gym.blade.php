6250dd5608720945f1ae1e5a046d493ac79eb84c
ScrubPotato <160197012+ScrubPotato@users.noreply.github.com>
2024-02-22 08:51:23 +0800

    Table added
    Added table, changed href for the gym landing page "book now" button. Added table css modifications.

--- resources/views/gym.blade.php
+++ resources/views/gym.blade.php
@@ -6,8 +6,12 @@
     <div class="banner-gym">
         <div class="container">
             <h1 class="banner-name">GYM RESERVATION</h1>
-            <a href="{{ route('gym') }}" class="btn btn-info btn-lg btn-block mt-2" data-mdb-ripple-init>Book Now</a>
+            <a href="#reservation-section" class="btn btn-info btn-lg btn-block mt-2" data-mdb-ripple-init>Book Now</a>
         </div>
     </div>
     
 @endsection
+@section('gym_table')
+@include('gym.gym_table')
+@endsection
+
