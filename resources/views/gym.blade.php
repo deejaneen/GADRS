50c07d5896c424bc703cd71ed17f4b0c39ba0ab4
ScrubPotato <160197012+ScrubPotato@users.noreply.github.com>
2024-02-22 08:55:33 +0800

    Update gym.blade.php

--- resources/views/gym.blade.php
+++ resources/views/gym.blade.php
@@ -2,16 +2,15 @@
 
 @section('banner')
 
-    <!-- Banner Section -->
-    <div class="banner-gym">
-        <div class="container">
-            <h1 class="banner-name">GYM RESERVATION</h1>
-            <a href="#reservation-section" class="btn btn-info btn-lg btn-block mt-2" data-mdb-ripple-init>Book Now</a>
-        </div>
+<!-- Banner Section -->
+<div class="banner-gym">
+    <div class="container">
+        <h1 class="banner-name">GYM RESERVATION</h1>
+        <a href="#reservation-section" class="btn btn-info btn-lg btn-block mt-2" data-mdb-ripple-init>Book Now</a>
     </div>
-    
+</div>
+
 @endsection
 @section('gym_table')
-@include('gym.gym_table')
+    @include('gym.gym_table')
 @endsection
-
