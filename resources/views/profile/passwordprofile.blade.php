34d0bb3abdbb65f4ffaa2994a79f50bafb5b6ce6
deeja-ub <tomeldendjaninetara@gmail.com>
2024-03-11 22:10:51 +0800

    view profile page

--- resources/views/profile/passwordprofile.blade.php
+++ resources/views/profile/passwordprofile.blade.php
@@ -3,6 +3,37 @@
 @section('profileview')
     <div class="profileview-container">
         <div class="left-column">
+            <div class="sidebar">
+                <a href="#" class="active">
+                    <span class="ri-user-line">
+                        <h3>Your Account</h3>
+                    </span>
+                </a>
+                <a href="#">
+                    <span class="ri-key-2-line">
+                        <h3>Change Password</h3>
+                    </span>
+                </a>
+                <a href="#">
+                    <span class="ri-receipt-line">
+                        <h3>Reservation History</h3>
+                    </span>
+                </a>
+                <a href="#">
+                    <span class="ri-logout-box-r-line">
+                        <h3>Logout</h3>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="right-column">
+            <div class="img-circle text-center">
+                <img src="{{asset('images/avatar.png')}}" alt="">
+            </div>
+            <hr>
+        </div>
+        
+        {{-- <div class="left-column">
            <div class="card-body pt-3">
             <ul class="nav nav-link-profile flex-column fw-bold gap-4">
                 <li class="nav-item">
@@ -26,6 +57,6 @@
                 euismod nulla id bibendum accumsan. Mauris suscipit consectetur tellus.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec velit eu est eleifend viverra. Nullam
                 euismod nulla id bibendum accumsan. Mauris suscipit consectetur tellus.</p>
-        </div>
+        </div> --}}
     </div>
 @endsection
