62a51868960dbe624b6ff3c51ab0df68b305e451
deeja-ub <tomeldendjaninetara@gmail.com>
2024-05-15 19:16:53 +0800

    test

--- resources/views/pdf/DormReservationFormSheet1.blade.php
+++ resources/views/pdf/DormReservationFormSheet1.blade.php
@@ -1940,6 +1940,11 @@
         }
 
         .c43 {
+            font-family: 'Calibri';
+            font-style: normal;
+            font-weight: 400;
+            src: url(https://fonts.gstatic.com/l/font?kit=J7afnpV-BGlaFfdAhLEY6w&skey=a1029226f80653a8&v=v15) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
             background-color: #ffffff;
             max-width: 612pt;
             padding: 8pt 8pt 8pt 8pt
