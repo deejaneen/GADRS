cffc979894e1e5c57bec6a23b2a476936a214b91
ScrubPotato <160197012+ScrubPotato@users.noreply.github.com>
2024-05-15 19:57:18 +0800

    Update DormReservationFormSheet1.blade.php

--- resources/views/pdf/DormReservationFormSheet1.blade.php
+++ resources/views/pdf/DormReservationFormSheet1.blade.php
@@ -2549,7 +2549,7 @@
         <tr class="">
             <td class="c56" colspan="3" rowspan="1">
                 <p class="c20 c107"><span class="c12 c1">RELATIONSHIP WITH GUEST: </span><span
-                        class="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$RELATIONSHIPWGUEST</span></p>
+                        class="c1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$RELATIONSHIPWGUEST</span></p>
             </td>
            
         </tr>
