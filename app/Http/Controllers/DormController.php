b6ed05eab80b48f9d20271fc315f24fd7459fe4f
ScrubPotato <160197012+ScrubPotato@users.noreply.github.com>
2024-06-12 18:48:23 +0800

    Last commit
    Minor changes in the Admin pages, since the logout popup modal is not working on other sections.

--- app/Http/Controllers/DormController.php
+++ app/Http/Controllers/DormController.php
@@ -34,51 +34,5 @@ class DormController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Dorm $dorm)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Dorm $dorm)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Dorm $dorm)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Dorm $dorm)
-    {
-        //
-    }
+   
 }
