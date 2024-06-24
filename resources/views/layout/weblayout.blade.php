bedabf449d1e79ed4357b95d25c4cb89835bb005
ScrubPotato <160197012+ScrubPotato@users.noreply.github.com>
2024-06-24 18:11:36 +0800

    Finished number 2.
    Halos sumabog na ang utak ko kakahanap ng solusyon.
    Iyong solusyon:
    style="display: none;"
    
    WOW! I love programming! WOHHH!

--- resources/views/layout/weblayout.blade.php
+++ resources/views/layout/weblayout.blade.php
@@ -18,8 +18,6 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
